28eac8c5360230f20d2a54db765675990b068b5e
denisesh <denisesh@gmail.com>
2014-11-10 11:24:12 -0800

    passed 01_welcome_menu_spec

--- lib/command_line_app.rb
+++ lib/command_line_app.rb
@@ -14,3 +14,18 @@ class CommandLineApp
     @output = output
   end
 end
+
+#   def get_input
+#     puts "What would you like to do?"
+#     gets.to_i
+#   end
+#
+#   input = get_input
+#   def run
+#     puts "Welcome"
+#     while input != "quit"
+#       puts "i don't do anything yet"
+#     end
+#     input = get_input
+#   end
+# end
--- lib/todo_app.rb
+++ lib/todo_app.rb
@@ -1,6 +1,31 @@
 class TodoApp < CommandLineApp
-  def initialize(input, output)
-    @input = input
-    @output = output
+
+  def real_puts message = " "
+    $stdout.puts message
   end
+
+  def run
+    puts "Welcome"
+    puts "'list' to list projects"
+    puts "'create' to create a new project"
+    puts "'edit' to edit a project"
+  end
+
+  input = gets.chomp
+  puts "Welcome"
+
+  def run
+    while input != "quit"
+      quit = gets.chomp
+      # if input == "list"
+      # puts "i'm inside list"
+      # end
+    end
+    puts "Type one of the commands"
+    input = gets.chomp
+  end
+
+
+
+
 end
