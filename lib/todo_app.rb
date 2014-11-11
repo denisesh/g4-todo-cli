d2f2c3f366cb30fba0117d8c36ebd46c88019c23
denisesh <denisesh@gmail.com>
2014-11-11 07:13:40 -0800

    03_projects_spec all pass

--- lib/todo_app.rb
+++ lib/todo_app.rb
@@ -2,55 +2,64 @@ class TodoApp < CommandLineApp
   def initialize(input, output)
     @input = input
     @output = output
+    @projects = []
   end
+
   def real_puts message = ""
     $stdout.puts message
   end
 
   def run
-    
     puts "Welcome"
     puts "'list' to list projects"
     puts "'create' to create a new project"
     puts "'edit' to edit a project"
 
-    user_input = gets.chomp
 
-    projects = []
+    user_input = gets.chomp.downcase
 
-    while user_input != "quit"
-      if user_input == "create"
-        puts "Please enter the new project name:"
-        @projects << gets.chomp
-      elsif user_input == "edit"
-        puts "Please enter the project name to edit:"
-        project_to_edit = gets.chomp
-        puts "Please enter the new project name:"
-        @projects[@projects.index(project_to_edit)] = gets.chomp
-      elsif gets == "list"
+    while user_input != 'quit'
+      if user_input == "list"
         list
-         #puts "Projects: \n none"
-      #else
-         #@projects.join(" ")
+      elsif user_input == "create"
+        create
+      elsif user_input == "edit"
+        edit
+      elsif user_input == "delete"
+        delete
       end
-      user_input = gets.chomp
-    end
 
-    def list
-      if projects.length == 0
-        puts "Projects:\n  none"
-      else
-        puts projects.join(" ")
-      end
+    user_input = gets.chomp
     end
+  end
 
-  # def display_projects
-  #   if @projects == []
-  #     "none"
-  #   else
-  #     @projects.join(" ")
-  #   end
-  #end
-#\n #{display_projects}
+  def delete
+    puts "Please enter the project name to delete:\n"
+    delete_project = gets.chomp
+    @projects.delete(delete_project)
+    #@projects[@projects.index(delete_project)]=nil
+  end
+
+  def edit
+    puts "Please enter the project name to edit:\n"
+    edit_project = gets.chomp
+    #something about calling create goes awry
+    puts "Please enter the new project name:\n"
+    new_name = gets.chomp
+    @projects[@projects.index(edit_project)]=new_name
+  end
+
+  def create
+    puts "Please enter the new project name:\n"
+    create_project = gets.chomp
+    @projects << create_project
+  end
+
+  def list
+    if @projects.empty?
+      puts "Projects:\n  none"
+    else
+      puts "Projects:\n  #{@projects.join(",")} "
+    end
   end
 end
